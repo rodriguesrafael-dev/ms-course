e4657ce398817e945f05e74da3439b9853cd1f92
rodriguesrafael <rodriguesrafael.dev@gmail.com>
2023-03-14 10:46:09 -0300

    Actuator for runtime config refresh

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hrpayroll.hrconfigserver;
+package com.devsuperior.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
